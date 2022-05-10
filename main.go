817c496267549b9a05142e1ffdf7c1681d422c7e
Yann Rolland <yann.rolland@iadvize.com>
2022-05-10 14:07:11 +0200

    wip

--- main.go
+++ main.go
@@ -302,7 +302,7 @@ func main() {
 		node_count++
 		bestNode := searchMCTS(&rootNode, myPlayer, 1000)
 		bestAction := bestNode.action
-		debug("bestAction", bestAction, "uct", uctMCTS(bestNode), "wins", bestNode.wins, "visits", bestNode.visits)
+		debug("bestAction", bestAction, showNode(bestNode))
 
 		fmt.Println(displayCoord(bestAction.From) + displayCoord(bestAction.To))
 		turn++
