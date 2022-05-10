60d2f0baea112670ca1906712bd6981ff343170c
Yann Rolland <yann.rolland@iadvize.com>
2022-05-10 23:24:38 +0200

    wip

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 	"time"
 )
 
-const DEBUG = true
+const DEBUG = false
 
 const MAX_TIME_MS_CG = 150
 const MAX_TIME_MS_LOCAL = 10 * 1000
-const ITERATIONS = 5
+const ITERATIONS = 1000
 
 var node_count = 0
 
